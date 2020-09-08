23767a7d9012e361d0a1bb22e915eff358c98225
yairazaria <yairazz12@gmail.com>
2020-09-08 15:32:32 +0300

    changed the design and added book search input

--- client/src/pages/BooksSearch/index.js
+++ client/src/pages/BooksSearch/index.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+
+import * as styles from './BooksSearch.module.scss';
+import { searchBooks } from "../../BooksApi/index";
+
 
 export const BooksSearch = () => {
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const [booksList, setBooksList] = useState([]);
+  const [resultNotFound, setResultNotFound] = useState('');
+
+
+  const getBooks = async (searchValue) => {
+    const res = await searchBooks(searchValue);
+    if (res && res.docs && res.docs.length > 0) {
+      const books = res.docs;
+      localStorage.setItem('books',JSON.stringify(books));
+      setBooksList(books);
+    }else{
+      setBooksList([]);
+      setResultNotFound('No Books Found');
+    }
+  };
+
+  const onChange = (e) => {
+    setSearchInputValue(e.target.value);
+  };
+  
+  const handaleKeyDown = (e) =>{
+    if (e.keyCode === 13) {
+      getBooks(searchInputValue);
+    };
+  };
+
+  console.log(booksList);
+
   return (
     <section>
+       <div className={styles.search_container}>
       <h1>Search books</h1>
+      <div className={styles.input_container}>
+      <input
+        className={styles.search_input}
+        type="text"
+        value={searchInputValue}
+        onChange={(e) => onChange(e)}
+        onKeyDown={handaleKeyDown}
+      />
+      </div>
+      </div>
     </section>
   );
 };
